55d03420c1a28c21cb04f8866d74478307b35298
MaindyG <fidelmaindygoue1@gmail.com>
2025-05-16 03:06:18 -0400

    Étapes préliminaires

---
+++ IdeaManager.UI/ViewModels/IdeaFormViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IdeaManager.Core.Entities;
+using IdeaManager.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace IdeaManager.UI.ViewModels
+{
+    public partial class IdeaFormViewModel : ObservableObject
+    {
+        private readonly IIdeaService _ideaService;
+
+        public IdeaFormViewModel(IIdeaService ideaService)
+        {
+            _ideaService = ideaService;
+        }
+
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private string description;
+
+        [ObservableProperty]
+        private string errorMessage;
+
+        [RelayCommand]
+        private async Task SubmitAsync()
+        {
+            try
+            {
+                var idea = new Idea
+                {
+                    Title = title,
+                    Description = description
+                };
+
+                await _ideaService.SubmitIdeaAsync(idea);
+                ErrorMessage = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
---
+++ IdeaManager.UI/ViewModels/IdeaListViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IdeaManager.Core.Entities;
+using IdeaManager.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaManager.UI.ViewModels
+{
+    internal class IdeaListViewModel :ObservableObject
+    {
+        private readonly IIdeaService _ideaService;
+
+        [ObservableProperty]
+        private ObservableCollection<Idea> _ideas;
+
+        public IdeaListViewModel(IIdeaService ideaService)
+        {
+            _ideaService = ideaService;
+            _ideas = new ObservableCollection<Idea>();
+            LoadAsync();
+        }
+
+        public async Task LoadAsync()
+        {
+            var ideas = await _ideaService.GetAllAsync();
+            if (ideas != null)
+            {
+                foreach (var idea in ideas)
+                {
+                    _ideas.Add(idea);
+                }
+            }
+        }
+    }
+}
